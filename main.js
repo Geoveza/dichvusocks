36bdaa351236c81336b6d321b032011a1b75120a
Ade Arman Wijaya <adearmanw@gmail.com>
2024-03-27 04:06:20 +0700

    update to fetch only residential

--- main.js
+++ main.js
@@ -24,11 +24,7 @@ function writeResultsToFile(data) {
 }
 // Replace with your cookies from the Cookie Editor
 const cookies = [
-  { "domain": "dichvusocks.net", "expirationDate": xxx, "hostOnly": true, "httpOnly": false, "name": "xxxx", "path": "/", "sameSite": "unspecified", "secure": false, "session": false, "storeId": "0", "value": "xxxx%3A4%3A%7Bi%3A0%3Bs%3A6%3A%22101602%22%3Bi%x", "id": 1 },
-  { "domain": "dichvusocks.net", "expirationDate": xxx, "hostOnly": true, "httpOnly": false, "name": "loginCookie", "path": "/", "sameSite": "unspecified", "secure": false, "session": false, "storeId": "0", "value": "x", "id": 2 },
-  { "domain": "dichvusocks.net", "expirationDate": xxx, "hostOnly": true, "httpOnly": false, "name": "notice", "path": "/", "sameSite": "unspecified", "secure": false, "session": false, "storeId": "0", "value": "1", "id": 3 },
-  { "domain": "dichvusocks.net", "expirationDate": Xxx, "hostOnly": true, "httpOnly": false, "name": "notice_time", "path": "/", "sameSite": "unspecified", "secure": false, "session": false, "storeId": "0", "value": "x", "id": 4 },
-  { "domain": "dichvusocks.net", "hostOnly": true, "httpOnly": false, "name": "PHPSESSID", "path": "/", "sameSite": "unspecified", "secure": false, "session": true, "storeId": "0", "value": "x", "id": 5 }
+paste cookiesnya
 ];
 
 const rl = readline.createInterface({
@@ -37,7 +33,6 @@ const rl = readline.createInterface({
 });
 
 const cookieHeader = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
-
 async function scrapeData() {
   try {
     const response = await axios.post(url, payload, {
@@ -63,45 +58,49 @@ async function scrapeData() {
       const country = cells[1].textContent.trim();
       const type = cells[10].textContent.trim();
 
-      data.push({
-        viewSocksId,
-        ipAddress,
-        country,
-        type
-      });
+      // Check if type is "hosting" or "Business", if so, skip this entry
+      if (type !== "Hosting" && type !== "Business") {
+        data.push({
+          viewSocksId,
+          ipAddress,
+          country,
+          type
+        });
+      }
     });
 
     console.log('Socks Found: Total', data.length);
 
-   rl.question('How many socks do you want? ', async (numSocks) => {
-    const results = [];
-    for (let i = 0; i < numSocks && i < data.length; i++) {
-      const { viewSocksId, ipAddress, country, type } = data[i];
-      console.log(`Accessing viewsocks for ${ipAddress} | ${country} | ${type}` );
+    rl.question('How many socks do you want? ', async (numSocks) => {
+      const results = [];
+      for (let i = 0; i < numSocks && i < data.length; i++) {
+        const { viewSocksId, ipAddress, country, type } = data[i];
+        console.log(`Accessing viewsocks for ${ipAddress} | ${country} | ${type}` );
 
-      const viewSocksUrl = `https://dichvusocks.net/viewsocks&id=${viewSocksId}`;
-      const viewSocksResponse = await axios.get(viewSocksUrl, {
-        headers: {
-          'Cookie': cookieHeader
+        const viewSocksUrl = `https://dichvusocks.net/viewsocks&id=${viewSocksId}`;
+        const viewSocksResponse = await axios.get(viewSocksUrl, {
+          headers: {
+            'Cookie': cookieHeader
+          }
+        });
+
+        const viewSocksHTML = viewSocksResponse.data;
+        const ipPortRegex = /show_socks_info\(\d+,"([^"]+)","([^"]+)"/;
+        const match = viewSocksHTML.match(ipPortRegex);
+
+        if (match) {
+          const [, ip, port] = match;
+          const socks5Proxy = `socks5://${ip}:${port}`;
+          console.log(`IP:Port for ${ipAddress}: ${socks5Proxy}`);
+          results.push({ipAddress, socks5Proxy}); // Menyimpan hasil ke array
+        } else {
+          console.log(`Failed to extract IP:Port for ${ipAddress}`);
+		          console.log("Response HTML:", viewSocksHTML);
         }
-      });
-
-      const viewSocksHTML = viewSocksResponse.data;
-      const ipPortRegex = /show_socks_info\(\d+,"([^"]+)","([^"]+)"/;
-      const match = viewSocksHTML.match(ipPortRegex);
-
-      if (match) {
-        const [, ip, port] = match;
-        const socks5Proxy = `socks5://${ip}:${port}`;
-        console.log(`IP:Port for ${ipAddress}: ${socks5Proxy}`);
-        results.push({ipAddress, socks5Proxy}); // Menyimpan hasil ke array
-      } else {
-        console.log(`Failed to extract IP:Port for ${ipAddress}`);
       }
-    }
-    writeResultsToFile(results); // Menulis hasil ke file
-    rl.close();
-  });
+      writeResultsToFile(results); // Menulis hasil ke file
+      rl.close();
+    });
   } catch (error) {
     console.error(error);
   }
